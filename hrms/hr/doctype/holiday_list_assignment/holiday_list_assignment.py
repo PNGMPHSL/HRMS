7b93fb21351dc58636af3d301c0b2df43bb80cac
Asmita Hase <asmitahase@gmail.com>
2025-09-15 16:50:24 +0530

    fix: validate overlapping holiday list assignment

--- hrms/hr/doctype/holiday_list_assignment/holiday_list_assignment.py
+++ hrms/hr/doctype/holiday_list_assignment/holiday_list_assignment.py
@@ -1,9 +1,32 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class HolidayListAssignment(Document):
-	pass
+	def validate(self):
+		self.validate_overlap_with_exisiting_assigment()
+
+	def validate_overlap_with_exisiting_assigment(self):
+		HLA = frappe.qb.DocType("Holiday List Assignment")
+		query = (
+			frappe.qb.from_(HLA)
+			.select(HLA.holiday_list)
+			.where((HLA.employee == self.employee) & (HLA.docstatus == 1))
+			.where(
+				(HLA.from_date.between(self.from_date, self.to_date))
+				or (HLA.to_date.between(self.from_date, self.to_date))
+				or (HLA.from_date >= self.start_date & HLA.to_date >= self.to_date)
+			)
+		).run()
+		holiday_list = query[0][0] if query else None
+
+		if holiday_list:
+			frappe.throw(
+				_(
+					"{0} already has an existing holiday list assigned for the selected period. Please edit the dates for current or eixsting assignment.\n {1}"
+				)
+			)
