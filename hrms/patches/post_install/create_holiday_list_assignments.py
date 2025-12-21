54125f05d0b264d60dfd21c62f1d7d487de07101
Asmita Hase <asmitahase@gmail.com>
2025-12-21 11:41:17 +0530

    fix: create holiday list assigment for company post istall

--- hrms/patches/post_install/create_holiday_list_assignments.py
+++ hrms/patches/post_install/create_holiday_list_assignments.py
@@ -1,37 +1,63 @@
+from pypika.terms import ValueWrapper
+
 import frappe
 
 
 def execute():
+	employee_holiday_details = get_employee_holiday_details()
+	company_holiday_details = get_company_holiday_details()
+
+	for entity in employee_holiday_details + company_holiday_details:
+		try:
+			create_holiday_list_assignment(entity)
+		except Exception as e:
+			frappe.log_error(e)
+
+
+def create_holiday_list_assignment(entity_details):
+	hla = frappe.new_doc("Holiday List Assignment")
+	hla.update(entity_details)
+	hla.save()
+	hla.submit()
+
+
+def get_employee_holiday_details():
 	employee = frappe.qb.DocType("Employee")
 	holiday_list = frappe.qb.DocType("Holiday List")
-
+	assigned_entity = ValueWrapper("Employee", "assigned_entity")
 	employee_holiday_details = (
 		frappe.qb.from_(employee)
 		.inner_join(holiday_list)
 		.on(employee.holiday_list == holiday_list.name)
 		.select(
-			employee.name,
+			(employee.name).as_("assigned_to"),
 			employee.holiday_list,
 			holiday_list.from_date,
 			holiday_list.to_date,
 			employee.company,
+			assigned_entity,
 		)
 		.where(employee.status == "Active")
 	).run(as_dict=True)
 
-	for employee in employee_holiday_details:
-		try:
-			create_holiday_list_assignment(employee)
-		except Exception as e:
-			frappe.log_error(e)
+	return employee_holiday_details
 
 
-def create_holiday_list_assignment(employee):
-	hla = frappe.new_doc("Holiday List Assignment")
-	hla.employee = employee.name
-	hla.company = employee.company
-	hla.holiday_list = employee.holiday_list
-	hla.from_date = employee.from_date
-	hla.to_date = employee.to_date
-	hla.save()
-	hla.submit()
+def get_company_holiday_details():
+	company = frappe.qb.DocType("Company")
+	holiday_list = frappe.qb.DocType("Holiday List")
+	assigned_entity = ValueWrapper("Company", "assigned_entity")
+	company_holiday_details = (
+		frappe.qb.from_(company)
+		.inner_join(holiday_list)
+		.on(company.default_holiday_list == holiday_list.name)
+		.select(
+			(company.name).as_("assigned_to"),
+			(company.default_holiday_list).as_("holiday_list"),
+			holiday_list.from_date,
+			holiday_list.to_date,
+			assigned_entity,
+		)
+	).run(as_dict=True)
+
+	return company_holiday_details
