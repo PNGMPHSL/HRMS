1b90c9b397d42d42ab5bc3ccd9590f7faf650d10
Asmita Hase <asmitahase@gmail.com>
2026-01-05 11:45:56 +0530

    fix: correct condition to fetch manually expired leaves
    (cherry picked from commit 723ea9d0e5b139e44ad66722529a6e5b750f81f4)

--- hrms/hr/doctype/leave_application/test_leave_application.py
+++ hrms/hr/doctype/leave_application/test_leave_application.py
@@ -1404,7 +1404,10 @@ class TestLeaveApplication(FrappeTestCase):
 		expire_allocation(leave_allocation, expiry_date=getdate())
 
 		leave_balance = get_leave_balance_on(
-			employee=employee.name, leave_type=leave_type.name, date=getdate()
+			employee=employee.name,
+			leave_type=leave_type.name,
+			date=getdate(),
+			consider_all_leaves_in_the_allocation_period=True,
 		)
 
 		self.assertEqual(leave_balance, 0)
@@ -1426,12 +1429,12 @@ class TestLeaveApplication(FrappeTestCase):
 			leave_type=leave_type,
 			from_date=previous_month_start,
 			to_date=previous_month_start,
-			posting_date=add_days(previous_month_end, -1),
+			posting_date=previous_month_end,
 			status="Approved",
 		)
 		doc.save()
 		doc.submit()
-		self.assertEqual(get_leave_balance_on(employee, leave_type, add_days(previous_month_end, -1)), 9)
+		self.assertEqual(get_leave_balance_on(employee, leave_type, previous_month_end), 9)
 
 	def test_status_on_discard(self):
 		make_allocation_record()
